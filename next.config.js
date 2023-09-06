3ed56b1e47cf89217db2373cf3867beaf955be6e
devconductor125 <stresscontroller9@gmail.com>
2023-09-06 09:06:36 +0800

    sdsdsd

--- next.config.js
+++ next.config.js
@@ -5,8 +5,12 @@ const nextConfig = {
     domains: ["https://coffy-next.netlify.app/", "localhost"],
     loader: "akamai",
     path: "",
+    remotePatterns: [
+      {
+        pathname: "/**",
+      },
+    ],
   },
-  NETLIFY_NEXT_PLUGIN_SKIP: true,
 };
 
 module.exports = nextConfig;
