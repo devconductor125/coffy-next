5456d800c2413075d6bd68c84b2a078a4da20680
devconductor125 <stresscontroller9@gmail.com>
2023-09-06 20:16:25 +0800

    sdsd
    sss

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     domains: ["*"],
     loader: "akamai",
     path: "",
     unoptimized: true,
   },
+  NETLIFY_NEXT_PLUGIN_SKIP: true,
   assetPrefix: "",
   compiler: {
     styledComponents: true,
